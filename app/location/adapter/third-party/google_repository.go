f545d5ffaf42e2276620793e4f9fc1e3bc3b82c4
muralish.clinton <muralish.clinton@circles.asia>
2023-03-04 21:41:25 +0530

    break down address to components

--- app/location/adapter/third-party/google_repository.go
+++ app/location/adapter/third-party/google_repository.go
@@ -2,6 +2,7 @@ package third_party
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/mcmuralishclint/location-service/app/location/domain"
 	"googlemaps.github.io/maps"
 )
@@ -33,10 +34,18 @@ func (r *GoogleMapsRepository) GetByID(id string) (*domain.Address, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	var result []map[string]interface{}
+	jsonData, err := json.Marshal(resp.AddressComponents)
+	json.Unmarshal([]byte(jsonData), &result)
+	addressComponents := ParseAddressComponents(result)
+
 	address = &domain.Address{
-		Type:             "google maps",
-		FormattedAddress: resp.FormattedAddress,
+		Type:              "google maps",
+		FormattedAddress:  resp.FormattedAddress,
+		AddressComponents: addressComponents,
 	}
+
 	r.cacheRepo.SetAddress(id, address)
 	return address, nil
 }
@@ -53,4 +62,32 @@ func (r *GoogleMapsRepository) QueryAutoComplete(input string) ([]domain.Autocom
 		autoCompletePredictions = append(autoCompletePredictions, domain.AutocompletePrediction{PlaceID: address.PlaceID, Description: address.Description})
 	}
 	return autoCompletePredictions, err
+}
+
+func ParseAddressComponents(components []map[string]interface{}) *domain.AddressComponents {
+	address := &domain.AddressComponents{}
+	for _, component := range components {
+		types := component["types"].([]interface{})
+		switch types[0] {
+		case "subpremise":
+			address.Subpremise = component["long_name"].(string)
+		case "street_number":
+			address.StreetNumber = component["long_name"].(string)
+		case "route":
+			address.Route = component["long_name"].(string)
+		case "locality":
+			address.Locality = component["long_name"].(string)
+		case "administrative_area_level_3":
+			address.AdministrativeArea3 = component["long_name"].(string)
+		case "administrative_area_level_2":
+			address.AdministrativeArea2 = component["short_name"].(string)
+		case "administrative_area_level_1":
+			address.AdministrativeArea1 = component["long_name"].(string)
+		case "country":
+			address.Country = component["long_name"].(string)
+		case "postal_code":
+			address.PostalCode = component["long_name"].(string)
+		}
+	}
+	return address
 }
--- app/location/domain/address.go
+++ app/location/domain/address.go
@@ -1,11 +1,24 @@
 package domain
 
 type Address struct {
-	Type             string
-	FormattedAddress string
+	Type              string
+	FormattedAddress  string
+	AddressComponents interface{}
 }
 
 type AutocompletePrediction struct {
 	Description string `json:"description,omitempty"`
 	PlaceID     string `json:"place_id,omitempty"`
+}
+
+type AddressComponents struct {
+	Subpremise          string `json:"subpremise"`
+	StreetNumber        string `json:"street_number"`
+	Route               string `json:"route"`
+	Locality            string `json:"locality"`
+	AdministrativeArea3 string `json:"administrative_area_level_3"`
+	AdministrativeArea2 string `json:"administrative_area_level_2"`
+	AdministrativeArea1 string `json:"administrative_area_level_1"`
+	Country             string `json:"country"`
+	PostalCode          string `json:"postal_code"`
 }
