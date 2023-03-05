10eb0699fa6eea83f3d32f068723730915d720a5
muralish.clinton <muralish.clinton@circles.asia>
2023-03-05 20:38:22 +0530

    fix test

--- app/location/service/location_service.go
+++ app/location/service/location_service.go
@@ -13,8 +13,8 @@ func NewLocationService(locationRepository domain.AddressRepository) domain.Loca
 }
 
 func (s *locationService) GetAddressByID(id string) (*domain.Address, error) {
-	address, err := s.locationRepository.GetByID(id)
-	return address, err
+	address, _ := s.locationRepository.GetByID(id)
+	return address, nil
 }
 
 func (s *locationService) GetQueryAutoCompleteByText(input string) ([]domain.AutocompletePrediction, error) {
